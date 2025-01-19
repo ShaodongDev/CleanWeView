6b7ad294cfd84eb6e641a5200c356a9cab314758
shaodong <iMac@example>
2025-01-19 16:14:32 +0100

    remove redundant URL check in bg.js

--- background.js
+++ background.js
@@ -3,29 +3,26 @@ chrome.runtime.onInstalled.addListener(() => {
     text: "OFF",
   });
 });
-
-const gzh = "https://mp.weixin.qq.com/s/";
+//When the extension is installed (or updated), this code will run, and it will set the badge on the extension’s icon to "OFF".
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(gzh)) {
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    const nextState = prevState === "ON" ? "OFF" : "ON";
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  const nextState = prevState === "ON" ? "OFF" : "ON";
 
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+
+  if (nextState === "ON") {
+    await chrome.scripting.insertCSS({
+      files: ["clean_we_view.css"],
+      target: { tabId: tab.id },
     });
-
-    if (nextState === "ON") {
-      await chrome.scripting.insertCSS({
-        files: ["clean_we_view.css"],
-        target: { tabId: tab.id },
-      });
-    } else if (nextState === "OFF") {
-      await chrome.scripting.removeCSS({
-        files: ["clean_we_view.css"],
-        target: { tabId: tab.id },
-      });
-    }
+  } else if (nextState === "OFF") {
+    await chrome.scripting.removeCSS({
+      files: ["clean_we_view.css"],
+      target: { tabId: tab.id },
+    });
   }
 });
