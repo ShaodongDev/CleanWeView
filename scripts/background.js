37963c0e1b452fb2d2f4e39fcfb1f7003e27f379
Shaodong-PC <zhanshaoton@gmail.com>
2025-02-19 11:25:32 +0100

    review bg and content.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
+  // oninstalled Fires When the Extension is Installed or Updated
   chrome.storage.sync.get("extensionEnabled", (result) => {
     const text = result.extensionEnabled ? "ON" : "OFF";
     chrome.action.setBadgeText({ text });
@@ -6,13 +7,16 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // onupdated fires when the page is loaded
   if (changeInfo.status === "complete") {
     chrome.storage.sync.get("extensionEnabled", (result) => {
       const text = result.extensionEnabled ? "ON" : "OFF";
       chrome.action.setBadgeText({ text, tabId: tabId });
+      // attention, argument passs to the previous API function
 
       if (result.extensionEnabled) {
         chrome.scripting.insertCSS({
+          // inset to DOM of target tab with tabID
           files: ["clean_we_view.css"],
           target: { tabId: tabId },
         });
