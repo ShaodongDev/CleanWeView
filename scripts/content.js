e749a3287d3c95cbf38a8157a5443bd726a421ff
Shaodong-iMac <zhanshaoton@gmail.com>
2025-02-24 19:28:40 +0100

    start redesign UI and add colored bg opts for v1.2

--- scripts/content.js
+++ scripts/content.js
@@ -7,5 +7,11 @@ chrome.storage.sync.get(["extensionEnabled", "pageWidth"], (result) => {
     link.rel = "stylesheet";
     link.href = chrome.runtime.getURL("clean_we_view.css");
     document.head.append(link);
+
+    // Remove background-color from all inline styles
+    const elements = document.querySelectorAll("*");
+    elements.forEach((element) => {
+      element.style.backgroundColor = "";
+    });
   }
 });
