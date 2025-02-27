cd79f1e21a24da81cf02cd73c269cc0eabc37a4b
Shaodong-iMac <zhanshaoton@gmail.com>
2025-02-27 09:54:07 +0100

    code cleanup

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,12 @@
 chrome.storage.sync.get(
   ["extensionEnabled", "pageWidth", "backgroundColor"],
   (result) => {
-    // The values of "extensionEnabled", "pageWidth", and "backgroundColor" are stored in the result object.
     if (result.extensionEnabled) {
       const width = result.pageWidth || 80;
       document.querySelector(
         "#page-content > div"
       ).style.maxWidth = `${width}%`;
-      // page-content > div selects only the div elements that are DIRECt children of #page-content.
 
-      // Apply the saved background color if it exists
       if (result.backgroundColor) {
         const pageContent = document.querySelector("#page-content");
         if (pageContent) {
@@ -18,7 +15,6 @@ chrome.storage.sync.get(
             `background-color: ${result.backgroundColor} !important`
           );
 
-          // Apply text color if dark mode is selected
           if (result.backgroundColor === "#222831") {
             document
               .querySelectorAll("#page-content, #page-content *")
@@ -29,11 +25,9 @@ chrome.storage.sync.get(
         }
       }
 
-      // this creates a new <link> element which is used to link css and add it to <head> via append
       const link = document.createElement("link");
       link.rel = "stylesheet";
       link.href = chrome.runtime.getURL("clean_we_view.css");
-      // .runtime to retrieve the current status of extenion including the structure tree so that the js knows where is the .css file
       document.head.append(link);
 
       // Remove original background-color from all inline styles
@@ -46,6 +40,3 @@ chrome.storage.sync.get(
     }
   }
 );
-
-// bg.js handles tasks that only need to checked once,like install refresh
-// content.js reacts to user cations on the page in real-time
